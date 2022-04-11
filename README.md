433eee28ac35034cb19a82a4de8b322aecca8500
Carlos Miguel Angulo Mascarell <cangulo@ohpen.com>
2022-04-11 14:37:40 +0200

    feat: LANZ-2249 improved gh action to accept custom conventional commits from a file

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
+- [generate-version-and-release-notes-gh-action](#generate-version-and-release-notes-gh-action)
+  - [Requirements](#requirements)
+  - [How to use](#how-to-use)
+    - [](#)
+- [License Summary](#license-summary)
+
 # generate-version-and-release-notes-gh-action
 
+## Requirements
+
+:warning: Attention! You need to merge your pull requests using the "squash" option. 
+
+![settings](docs/how_to_merge_prs.png)
+
+You have to update your repo to only allow that `squash and merge`:
+
+![settings](docs/gh_repo_merge_settings.png)
+
+## How to use
 This repository includes an action to semantically version your repository once a merge happens to the main branch. This is an example on how to use the action in your own repository:
 
 ```yaml
@@ -30,10 +47,7 @@ jobs:
 The action will:
 
 - Analyse the commits from the pull request that has been merged to main branch and extract the necessary information.
-
-:warning: Attention! You need to merge pull requests in a way their commits are present in the merge commit. To do so, merge your pull request as "squash". Check the section [GitHub Repository Settings required](#github-repository-settings-required).
-
-- Summarise all the pull request changes into you CHANGELOG.md file.
+- Summarize all the pull request changes into you CHANGELOG.md file.
 - Deduce the new version from those commits (your commits must follow conventional-commits! Check out the _check-conventional-commits_ action).
 - Commit, tag and push changes in version.json and CHANGELOG.md (you can skip this part by setting parameter _skip-git-commit_ to true, for example when you want to change more files and push changes in one commit by yourself)
 - You can also set up name to sign the commit with parameter: _user-name_. Default value is _GitHub Actions_
@@ -50,11 +64,81 @@ The action will:
 > **skip-commit**: use it with value "true" if you want to prevent the action from commiting.
 > **version-prefix**: use with a value different than an empty string ("beta-" or "v" for example) to have tags in the form of '{version-prefix}M.m.p'
 
-# GitHub Repository Settings required
+### Commits that increase the version:
 
-Please set the next settings in your repository to ensure this GH action can work:
+The next commit types (prefixes) define the release type (major, minor, patch) when a PR is merged:
 
-![settings](docs/gh_repo_merge_settings.png)
+```json
+[
+    {
+        "commitType": "break",
+        "releaseType": "major"
+    },
+    {
+        "commitType": "feat",
+        "releaseType": "minor"
+    },
+    {
+        "commitType": "fix",
+        "releaseType": "patch"
+    }
+]
+```
+examples:
+
+* break: LANZ-123 updated API desing-> create a major release (**X+1**.y.z)
+* feat: LANZ-123 created new feature -> create a minor release (x.**Y+1**.z)
+* fix: LANZ-123 fixed bug keeping compatibility-> create a patch release (x.y.**Z+1**)
+
+### Remarks about your versioning!
+
+* If your version.json file has a 3 number version (`major.minor.patch`), others prefixes won't create a release
+* If your has a 4 number version (`major.minor.patch.extra`), others prefixes will increase the `extra` digit.
+
+### You want to use custom conventional commits?
+
+Follow the next steps to define more conventional commits:
+
+1. Create a json file providing the custom commit type and the release associated (`major,minor,patch`) as next:
+
+```json
+[
+    {
+        "commitType": "break",
+        "releaseType": "major"
+    },
+    {
+        "commitType": "feat",
+        "releaseType": "minor"
+    },
+    {
+        "commitType": "fix",
+        "releaseType": "minor"
+    },
+    {
+        "commitType": "refactor",
+        "releaseType": "minor"
+    },
+    {
+        "commitType": "docs",
+        "releaseType": "minor"
+    }
+]
+```
+
+2. Provide its path in the parameter `custom-conventional-commits-file`
+
+```yaml
+      - uses: ohpensource/generate-version-and-release-notes-gh-action@main
+        name: semver & changelog
+        with:
+          user-email: "user@email.com"
+          skip-commit: "true"  This is for testing so you don't polute your git history. Default value is false.
+          version-prefix: "v"  useful for repos that terraform modules where the versions are like "v0.2.4".
+          custom-conventional-commits-file: custom-conventional-commits.json
+```
+example fie: [custom-conventional-commits-accepted.json](custom-conventional-commits-accepted.json)
+
 
 # License Summary
 
--- generate-version-and-release-notes.js
+++ generate-version-and-release-notes.js
@@ -4,52 +4,76 @@ const git = require("./git.js");
 const logger = require("./logging.js");
 const files = require("./file-tools.js");
 
+logger.logTitle("GENERATE VERSION AND RELEASE NOTES");
+
+// -----------------------------//
+// ----- INPUT PARAMETERS ----- //
+// -----------------------------//
+
 const skipGitCommit = process.argv[2];
 const versionPrefix = process.argv[3];
+const customCCFilePath = process.argv[4];
+const defaultCCFilePath = process.env.DEFAULT_CC
+defaultCCTypes = parseFile(defaultCCFilePath)
 
-const featPreffix = "feat:";
-const fixPreffix = "fix:";
-const breakPreffix = "break:";
-const versionFile = "version.json";
-const changelogFile = "CHANGELOG.md";
+logger.logKeyValuePair("skip-git-commit", skipGitCommit);
+logger.logKeyValuePair("version-prefix", versionPrefix);
+logger.logKeyValuePair("custom-conventional-commits-file", customCCFilePath);
+logger.logKeyValuePair("default-cc-types", defaultCCTypes);
 
 // ------------------ //
 // ----- SCRIPT ----- //
 // ------------------ //
-logger.logAction("GETTING MERGE COMMIT");
+
+const versionFile = "version.json";
+const changelogFile = "CHANGELOG.md";
+
+let useCustomCC = false
+let customCCTypes = []
+if (customCCFilePath) {
+  useCustomCC = true;
+  customCCTypes = parseFile(customCCFilePath);
+  logger.logKeyValuePair("custom-cc-types", customCCTypes);
+}
+
+logger.logKeyValuePair("using-custom-conventional-commits", useCustomCC);
+const commitTypes = useCustomCC ? customCCTypes : defaultCCTypes
+
+logger.logTitle("GETTING MERGE COMMIT");
 let lastCommit = git.getLastCommit();
 logger.logKeyValuePair("commit", lastCommit);
 
-logger.logAction("GETTING CHANGES");
+logger.logTitle("GETTING CHANGES");
 let changes = lastCommit.body
   .split("\n")
   .filter((block) => block.startsWith("* "))
-  .map((block) => getChange(block.replace("* ", "")));
+  .map((block) => getChange(block.replace("* ", ""), commitTypes));
+
 if (changes.length === 0) {
-  changes.push(getChange(lastCommit.subject));
+  changes.push(getChange(lastCommit.subject, commitTypes));
 }
 logger.logKeyValuePair("changes", changes);
 
-logger.logAction("GETTING PREVIOUS VERSION");
+logger.logTitle("GETTING PREVIOUS VERSION");
 let versionFileContent = files.getJsonFrom(versionFile);
 let previousVersion = getPreviousVersionAsText(versionFileContent);
 logger.logKeyValuePair("previous-version", previousVersion);
 
-logger.logAction("GETTING NEW VERSION");
+logger.logTitle("GETTING NEW VERSION");
 let newVersion = getUpdatedVersion(previousVersion, changes);
 logger.logKeyValuePair("new-version", newVersion);
 
-logger.logAction("UPDATING VERSION FILE");
+logger.logTitle("UPDATING VERSION FILE");
 files.saveJsonTo(
   versionFile,
   files.prettifyJsonObject({ version: newVersion })
 );
 
-logger.logAction("UPDATING CHANGELOG FILE");
+logger.logTitle("UPDATING CHANGELOG FILE");
 updateChangelogFile(newVersion, changes);
 
 if (skipGitCommit !== "true") {
-  logger.logAction("COMMITTING AND TAGGING");
+  logger.logTitle("COMMITTING AND TAGGING");
   commitAndTag(`${versionPrefix}${newVersion}`);
 }
 
@@ -79,18 +103,18 @@ function getUpdatedVersion(version, changes) {
   let newMinor = 0;
   let newPatch = 0;
   let newSecondary = 0;
-  if (changes.some((change) => change.type === "break")) {
+  if (changes.some((change) => change.type === "major")) {
     newMajor = major + 1;
     newMinor = 0;
     newPatch = 0;
     newSecondary = 0;
-  } else if (changes.some((change) => change.type === "feat")) {
+  } else if (changes.some((change) => change.type === "minor")) {
     newMajor = major;
     newMinor = minor + 1;
     newPatch = 0;
     newSecondary = 0;
   } else if (
-    changes.some((change) => change.type === "fix") ||
+    changes.some((change) => change.type === "patch") ||
     versionFileContent.length === 3
   ) {
     newMajor = major;
@@ -110,27 +134,27 @@ function getUpdatedVersion(version, changes) {
     return `${newMajor}.${newMinor}.${newPatch}.${newSecondary}`;
   }
 }
-function getChange(line) {
-  if (line.startsWith(featPreffix)) {
-    return {
-      type: featPreffix.replace(":", ""),
-      content: line.replace(featPreffix, "").trim(),
-    };
-  } else if (line.startsWith(fixPreffix)) {
-    return {
-      type: fixPreffix.replace(":", ""),
-      content: line.replace(fixPreffix, "").trim(),
-    };
-  } else if (line.startsWith(breakPreffix)) {
-    return {
-      type: breakPreffix.replace(":", ""),
-      content: line.replace(breakPreffix, "").trim(),
-    };
+function getChange(line, commitTypes) {
+  let type = "";
+  let content = "";
+
+  if (line.split(":").length >= 2) {
+    const prefix = line.split(":")[0].trim();
+    content = line.split(":")[1].trim();
+    const validPrefix = commitTypes.some(x => x.commitType == prefix);
+
+    type = validPrefix ? commitTypes
+      .find(x => x.commitType == prefix)
+      .releaseType : "none"
+
   } else {
-    return {
-      type: "none",
-      content: line.trim(),
-    };
+    type = "none"
+    content = line.trim()
+  }
+
+  return {
+    type,
+    content
   }
 }
 function getPreviousVersionAsText(versionFileContent) {
@@ -149,21 +173,21 @@ function updateChangelogFile(newVersion, changes) {
     changelog,
     "## :boom: BREAKING CHANGES\n",
     changes
-      .filter((change) => change.type == "break")
+      .filter((change) => change.type == "major")
       .map((change) => `* ${change.content}\n`)
   );
   changelog = updateChangelogWith(
     changelog,
     "## :hammer: Features\n",
     changes
-      .filter((change) => change.type == "feat")
+      .filter((change) => change.type == "minor")
       .map((change) => `* ${change.content}\n`)
   );
   changelog = updateChangelogWith(
     changelog,
     "## :bug: Fixes\n",
     changes
-      .filter((change) => change.type == "fix")
+      .filter((change) => change.type == "patch")
       .map((change) => `* ${change.content}\n`)
   );
   changelog = updateChangelogWith(
@@ -190,4 +214,12 @@ function commitAndTag(tagContent) {
   child.execSync(`git commit -m "[skip ci] Bump to version ${tagContent}"`);
   child.execSync(`git tag -a -m "Tag for version ${tagContent}" ${tagContent}`);
   child.execSync(`git push --follow-tags`);
+}
+
+// --------------------- //
+// ----- FUNCTIONS ----- //
+// --------------------- //
+function parseFile(filePath) {
+  const rawdata = fs.readFileSync(filePath);
+  return JSON.parse(rawdata);
 }
